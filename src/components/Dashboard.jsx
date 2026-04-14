51ef5df5dc40795d28291c8e92fa0acb26cd6523
Sirwin2win <sirwin2win@gmail.com>
2026-04-14 12:44:52 +0100

    Mentenance started

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import admin from '../assets/images/admin.avif'
-import { FaBell } from 'react-icons/fa'
+import { FaBell, FaTools, FaArrowDown , FaPowerOff   } from 'react-icons/fa'
 import { MdPayment } from "react-icons/md";
+import { IoDocumentTextSharp } from "react-icons/io5";
+import { GiSpanner } from "react-icons/gi";
+
+
 
 
 
@@ -35,6 +39,71 @@ const Dashboard = () => {
                   <p className='text-[#223B7E] ms-3'>Make a Payment</p>
                 </button>
 
+              </div>
+              {/* Quick Actions Div */}
+              <div className='bg-[#F5F5F5] px-5'>
+                <p className='text-2xl font-bold pt-10'>Quick Actions</p>
+
+                <div className='flex justify-between mt-5'>
+                  {/* Payment button */}
+               <button className='flex justify-center bg-white rounded-lg p-2'>
+                  <MdPayment className='text-[#223B7E] size-7 bg-gray-200 p-1' /> 
+                  <p className='text-[#223B7E] ms-3'>Make a Payment</p>
+                </button>
+                {/* Submit mentenance request button */}
+                 <button className='flex justify-center bg-white rounded-lg p-2'>
+                  <FaTools  className='text-[#223B7E] size-7 bg-gray-200 p-1' /> 
+                  <p className='text-[#223B7E] ms-3'>Make a Payment</p>
+                </button>
+                {/* View Lease details */}
+                 <button className='flex justify-center bg-white rounded-lg p-2'>
+                  <IoDocumentTextSharp className='text-[#223B7E] size-7 bg-gray-200 p-1' /> 
+                  <p className='text-[#223B7E] ms-3'>Make a Payment</p>
+                </button>
+                </div>
+
+              {/* Recent Activity */}
+              <div className='flex justify-between'>
+                <div>
+                  <p className='text-2xl font-bold pt-10'>Recent Activity</p>
+                  <div className='flex justify-between bg-white p-2 rounded-lg mt-5'>
+                      <div className='flex justify-center'>
+                        <FaArrowDown className='bg-[#00FF3C33] text-[#00A725] size-10 rounded-full p-2 me-3' />
+                        <div>
+                          <p className='text-lg font-bold'>Rent Payment Received</p>
+                          <p className='text-gray-500'>June 1, 2025</p>
+                        </div>
+
+                      </div>
+                      <p className='text-[#00A725] ms-20'>-$1,240.00</p>
+                  </div>
+                  <div className='flex justify-between bg-white p-2 rounded-lg mt-5'>
+                      <div className='flex justify-center'>
+                        <GiSpanner className='bg-[#FF670033] text-[#FF6700] size-10 rounded-full p-2 me-3' />
+                        <div>
+                          <p className='text-lg font-bold'>Maintenance Request Submitted</p>
+                          <p className='text-gray-500'>May 1, 2025 - Leaky Faucet</p>
+                        </div>
+
+                      </div>
+                      {/* <p className='text-[#00A725] ms-20'>-$1,240.00</p> */}
+                  </div>
+
+                </div>
+                {/* Autopay Status */}
+                <div className='bg-white p-4 mt-5 rounded-lg'>
+                  <p className='text-2xl font-bold'>Autopay Status</p>
+                  <div className='flex justify-evenly mt-5'>
+                    <FaPowerOff className='bg-[#D8181833] text-[#D81818] p-2 size-10 rounded-full' />
+                    <div className='ms-3'>
+                      <p className='text-lg font-bold'>Autopay status is inactive</p>
+                      <p className='text-gray-500'>Enable to pay bills automatically</p>
+                    </div>
+
+                  </div>
+                  <button className='bg-[#FF6700C9] text-[#223B7E] rounded-lg p-3 w-full font-bold mt-6 mb-15'>Manage Autopay</button>
+                </div>
+              </div>
               </div>
 
     </div>
--- src/components/Mentenance.jsx
+++ src/components/Mentenance.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Mentenance = () => {
   return (
-    <div>
-        <h1>Mentenance Page</h1>
+    <div className='p--5'>
+        <p className='text-2xl font-bold'>Maintenance Requests</p>
+        <p className='text-gray-500'>Submit and track maintenance request for your property</p>
+          {/* Two Splitted Divs */}
+        <div className='flex justify-between my-10'>
+          <form className='bg-[#F5F5F5] py-5 px-3'>
+            <p className='text-xl font-bold'>Submit a New Request</p>
+              <p className='text-gray-500 mt-3 mb-10'>Please fill out the form below. Provide as much details as 
+                <span className='block'>possible. </span>
+                </p>
+
+                <div>
+                  <label htmlFor="category" className='text-lg font-700 block'>Category/Type of issue</label>
+                  <select name="" id="" className='border border-gray-400 w-full my-5 h-15 rounded-lg'>
+                    <option value="" className=''>Select an issue type</option>
+                  </select>
+                </div>
+
+                <div>
+                  <label htmlFor="category" className='text-lg font-700 block'>Subject</label>
+                  <input type="text" placeholder='e.g leaky faucet in the kitchen' 
+                  className='border border-gray-400 w-full my-5 h-15 rounded-lg placeholder:ps-5' />
+                </div>
+
+                <div>
+                  <label htmlFor="description" className='text-lg font-700 block'>Subject</label>
+                  <textarea name="description" id="description"
+                   className='border border-gray-400 w-full my-5 h-45 rounded-lg placeholder:ps-5'>
+
+                  </textarea>
+                </div>
+          </form>
+        </div>
     </div>
   )
 }
