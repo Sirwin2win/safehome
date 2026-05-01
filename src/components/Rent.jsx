4d59e8cf0812c1e247e55752735924ca913c20b2
Sirwin <sirwin2win@gmail.com>
2026-05-01 21:28:47 +0100

    update

--- src/components/Rent.jsx
+++ src/components/Rent.jsx
@@ -2,38 +2,39 @@ import React from 'react'
 
 const Rent = () => {
   return (
-    <form>
+    <form >
         <p className='text-2xl font-bold my-5'>Home Type</p>
 
-        <div className='inline md:block'>
+        <div className='inline md:block ps-2'>
             <input type="checkbox" />
             <label htmlFor="apartmen" className='ms-3 text-xl font-[400]'>Apartment</label>
         </div>
 
         <div className='my-5 inline md:block'>
-            <input type="checkbox" />
+            <input type="checkbox" className='ms-2' />
             <label htmlFor="condo" className='ms-3 text-xl font-[400]'>Condo</label>
         </div>
 
         <div className='inline md:block'>
-            <input type="checkbox" />
-            <label htmlFor="duplex" className='ms-3 text-xl font-[400]'>Duplex</label>
+            <input type="checkbox" className='ms-2'/>
+            <label htmlFor="duplex" className='ms-2 text-xl font-[400]'>Duplex</label>
         </div>
-
-        <div className='my-5 inline md:block'>
-            <input type="checkbox" />
+            <br />
+        <div className='my-5 inline md:block ms-4'>
+            <input type="checkbox" className='ms-1' />
             <label htmlFor="villa" className='ms-3 text-xl font-[400]'>Villa</label>
         </div>
 
          <p className='text-2xl font-bold my-5'>Home Status</p>
          
-        <div className='grid grid-cols-2 gap-4'>
+        {/* <div className=''> */}
+        <div className='flex md:grid md:grid-cols-2 md:gap-4'>
             <div className='my-5 px-2 flex justify-center border border-gray-300 rounded-full'>
             <input type="radio" />
             <label htmlFor="rent" className='ms-3 text-xl font-[400]'>Rent</label>
         </div>
          
-        <div className='my-5 px-2 flex justify-center border border-gray-300 rounded-full'>
+        <div className='my-5 px-2 flex justify-center border border-gray-300 rounded-full mx-5 md:mx-0'>
             <input type="radio" />
             <label htmlFor="rent" className='ms-3 text-xl font-[400]'>Buy</label>
         </div>
